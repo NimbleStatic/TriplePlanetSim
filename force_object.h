62963d011f78fd0e9a5510e399110867588ac6d2
Gustaw Malinowski <gustaw.ggm@gmail.com>
2023-07-19 21:18:50 +0200

    added force_object header file

--- force_object.h
+++ force_object.h
@@ -1,13 +1,33 @@
 #ifndef PLANET_OBJECT_H
 #define PLANET_OBJECT_H
-#include "position.h"
+#include "structs.h"
 #include"vector"
 #include"tuple"
 
 class Force_Object{
     private:
-    Position current_positon;
-    std::vector<std::tuple<Position,
+        long double mass;
+        long double size;
+        Position current_positon;
+        Force current_forces;
+        Velocity current_velocity;
+        std::vector<std::tuple<Position,Time>> pos_history;
+    public:
+        Force_Object(long double mass, long double size);
+        void set_force(long double Fx, long double Fy, long double Fz);
+        void add_force(long double Fx, long double Fy, long double Fz);
+        void set_velocity(long double Vx, long double Vy, long double Vz);
+        void add_velocity(long double Vx, long double Vy, long double Vz);
+        void set_force(Force value);
+        void add_force(Force value);
+        void set_velocity(Velocity value);
+        void add_velocity(Velocity value);
+        void move_in_time(Time moving_time);
+        long double get_mass() const;
+        long double get_size() const;
+        Position get_current_pos() const;
+        std::vector<std::tuple<Position,Time>> get_pos_history() const;
+
 
 
 };
--- structs.h
+++ structs.h
@@ -1,6 +1,7 @@
 #ifndef STRUCTS_H
 #define STRUCTS_H
 
+
 struct Time{
     long double time;
 };
@@ -12,6 +13,18 @@ struct Position{
     long double z;
 
     long double get_distance_from_pos(Position &value) const;
+};
+
+struct Force{
+    long double x;
+    long double y;
+    long double z;
+};
+
+struct Velocity{
+    long double x;
+    long double y;
+    long double z;
 };
 
 #endif
