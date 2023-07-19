a483c11ba0d3ab058684cdec479295e46955776e
Gustaw Malinowski <gustaw.ggm@gmail.com>
2023-07-19 17:36:08 +0200

    added time struct

---
+++ force_object.h
@@ -0,0 +1,15 @@
+#ifndef PLANET_OBJECT_H
+#define PLANET_OBJECT_H
+#include "position.h"
+#include"vector"
+#include"tuple"
+
+class Force_Object{
+    private:
+    Position current_positon;
+    std::vector<std::tuple<Position,
+
+
+};
+
+#endif
---
+++ relative_time.h
@@ -0,0 +1,8 @@
+#ifndef TIME_H
+#define TIME_H
+
+struct Time{
+    long double time;
+};
+
+#endif
